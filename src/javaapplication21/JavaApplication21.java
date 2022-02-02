2a9c092a9fe4b66d373641dc66d7310d9a00abaa
Owner <Owner@192.168.2.22>
2022-02-02 10:55:12 -0600

    added message "This is the 2nd test""

--- src/javaapplication21/JavaApplication21.java
+++ src/javaapplication21/JavaApplication21.java
@@ -4,6 +4,7 @@ public class JavaApplication21 {
 
     public static void main(String[] args) {
         System.out.println("This is a test");
+        System.out.println("This is the 2nd test");
     }
     
 }
